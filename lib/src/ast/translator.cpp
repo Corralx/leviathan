c57dc5b59294ca50e810eb460e9812ef93e8c67e
Alex Falcon <alex930605@gmail.com>
2018-01-01 22:05:23 +0100

    In S and P axioms I was considering them as strict (e.g. P semantics: exists j <= i ... but I was considering: exists j < i ..., thus the initial condition was \lnot p), but in the paper a non-strict semantics is used. Thus, both S and P initial conditions (respectively, \lnot p, \lnot s) are now set differently (respectively: p \iff \phi i.e. P \phi is true at state 0 iff \phi is true, and similarly s \iff \phi_2)

--- lib/src/ast/translator.cpp
+++ lib/src/ast/translator.cpp
@@ -91,7 +91,7 @@ FormulaPtr Translator::translate_p(FormulaPtr f, FormulaPtr& a, std::set<std::st
     FormulaPtr newf = make_atom(prop_name(f, ps));
     format::debug("--- introduced propositional letter: {}", p.to_string(newf));
     // then, modify the current axioms adding the adhoc axioms which regulate the newly added propositional letter's semantics (and these axioms are modified as side effect, so there's no need to return them)
-    a = conc(a, make_conjunction(make_negation(newf), make_always(make_iff(make_tomorrow(newf), make_conjunction(make_tomorrow(fpl), make_disjunction(newf, fpr))))));
+    a = conc(a, make_conjunction(make_iff(newf, fpr), make_always(make_iff(make_tomorrow(newf), make_conjunction(make_tomorrow(fpl), make_disjunction(newf, fpr))))));
     format::verbose("{} ->    {}", p.to_string(f), p.to_string(a));
     // finally, we return the propositional letter which took the role of f (Since sub)
     return newf;
@@ -124,7 +124,7 @@ FormulaPtr Translator::translate_p(FormulaPtr f, FormulaPtr& a, std::set<std::st
     FormulaPtr newf = make_atom(prop_name(f, ps));
     format::debug("--- introduced propositional letter: {}", p.to_string(newf));
     // then, modify the current axioms adding the adhoc axioms which regulate the newly added propositional letter's semantics (and these axioms are modified as side effect, so there's no need to return them)
-    a = conc(a, make_conjunction(make_negation(newf), make_always(make_iff(make_tomorrow(newf), make_disjunction(newf, fp))))); 
+    a = conc(a, make_conjunction(make_iff(newf, fp), make_always(make_iff(make_tomorrow(newf), make_disjunction(newf, fp))))); 
     format::verbose("{} ->    {}", p.to_string(f), p.to_string(a));
     // finally, we return the propositional letter which took the role of f (Past sub)
     return newf;
